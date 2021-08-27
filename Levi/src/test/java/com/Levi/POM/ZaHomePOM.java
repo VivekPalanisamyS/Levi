0682e392406bc07fa74dbea70448253479e33d5c
vivek <vivek@DESKTOP-UVQNT7S>
2021-08-27 10:01:12 +0530

    Linked Hash Map V1.1

--- Levi/src/test/java/com/Levi/POM/ZaHomePOM.java
+++ Levi/src/test/java/com/Levi/POM/ZaHomePOM.java
@@ -1,13 +1,21 @@
 package com.Levi.POM;
 
+import java.util.List;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class ZaHomePOM {
 	
 	@FindBy(xpath="//div[@id='fb-root']/following-sibling::header//div[@id='roadblock-container']//button[contains(@class,'close')]")
-	public static WebElement frameClose;
+	public static WebElement popupClose;
 	@FindBy(xpath="//input[@id='search-field']")
 	public static WebElement search;
+	
+	@FindBy(xpath="//div[@id='fb-root']/following-sibling::header//li[contains(@class,'dropdown dropdown__full navbar-sub')]//a[@class='navbar-link']")
+	public static List<WebElement> mainLink;
+	
+	@FindBy(xpath="//div[@id='fb-root']/following-sibling::header//li[contains(@class,'dropdown dropdown__full navbar-sub')]//a[contains(@class,'item-title')]")
+	public static List<WebElement> itemTitle;
 
 }
--- Levi/src/test/java/com/Levi/Test/ZaHomeTest.java
+++ Levi/src/test/java/com/Levi/Test/ZaHomeTest.java
@@ -1,7 +1,11 @@
 package com.Levi.Test;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Logger;
@@ -9,6 +13,8 @@ import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -19,21 +25,40 @@ import com.Levi.POM.ZaHomePOM;
 import com.Levi.Utilities.CommonUtilsLevi;
 
 public class ZaHomeTest extends CommonUtilsLevi {
+
+	Logger logger = Logger.getLogger(ZaHomeTest.class);
+	Map<String, WebElement> links = new LinkedHashMap<String, WebElement>();
 	
-	Logger logger=Logger.getLogger(ZaHomeTest.class);
-	@Test
-	public void ZaHomeTestMethod()
-	{
-		PageFactory.initElements(CommonUtilsLevi.driver, ZaHomePOM.class);
-		 
-		WebDriverWait wait2 = new WebDriverWait(driver,10);
-		wait2.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='fb-root']/following-sibling::header//div[@id='roadblock-container']//button[contains(@class,'close')]")));
-		ZaHomePOM.frameClose.click();
-		/*
-		 * JavascriptExecutor j = (JavascriptExecutor) driver;
-		 * j.executeScript("arguments[0].click()",ZaHomePOM.frameClose);
-		 */
-		 ZaHomePOM.search.sendKeys("success dress");
+	public void findLinks(List<WebElement> link) {
+		for (WebElement li : link) {
+			if(li.getText()!=null)
+			links.put(li.getText(), li);
+		}
+		System.out.println(links.keySet());
 	}
 
+	private void mouseOver(String title) {
+		Actions action=new Actions(driver);
+		action.moveToElement(driver.findElement(By.linkText(title))).perform();
+		WebDriverWait wait2 = new WebDriverWait(driver, 10);
+		wait2.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(
+				"//div[@id='fb-root']/following-sibling::header//li[contains(@class,'dropdown dropdown__full navbar-sub')]//a[contains(@class,'item-title')]")));
+		findLinks(ZaHomePOM.itemTitle);
+	}
+
+
+	@Test
+	public void ZaHomeTestMethod() {
+		PageFactory.initElements(CommonUtilsLevi.driver, ZaHomePOM.class);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		WebDriverWait wait2 = new WebDriverWait(driver, 10);
+		wait2.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(
+				"//div[@id='fb-root']/following-sibling::header//div[@id='roadblock-container']//button[contains(@class,'close')]")));
+		ZaHomePOM.popupClose.click();
+		// JavascriptExecutor j = (JavascriptExecutor)driver;j.executeScript("arguments[0].click()",ZaHomePOM.frameClose);
+		findLinks(ZaHomePOM.mainLink);
+		for(String title : links.keySet())
+		mouseOver(title);
+		System.out.println(links.keySet());
+	}
 }
